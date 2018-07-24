7588340b0a90ba68a005691b3e693f02683a4962
Diego Schell <dschell@conae.gov.ar>
2018-07-24 14:10:09 -0300

    subo el script para raster vs datos cosechadora

---
+++ fenoRVI.R
@@ -0,0 +1,65 @@
+library(raster)
+library(rgdal)
+library(ggplot2)
+library(GGally)
+setwd("~/owncloud/7-aplicaciones")
+#camino <- "imagenes/indices/20171116T141930-indices.tif"
+
+# Defino que imagenes abrir de los indices
+#files <- list.files(path = camino, pattern = '.tif$', full.names = TRUE)
+#indices <- stack(files)
+# Abro la imagen cosmo
+indices <- stack("campoCONAE/imagenes/indices/20171116T141930-indices.tif")
+names(indices) <- c("arvi", "dvi", "gemi", "gndvi", "ipvi", "ireci", "mcari", "msavi2", "msavi", "mtci", "ndi45", "pssra", "pvi", "reip", "rvi", "s2rep", "savi", "tndvi", "tsavi", "wdvi", "NDVI")
+
+
+plot(values(indices$arvi), values(indices$gemi))
+cor(values(indices$arvi), values(indices$gemi))
+indices <- crop(indices, extent(cosmo))
+
+# Apilo todo
+
+
+# Cargo el vector para hacer el muestreo
+#poligonoextraccion <- readOGR(dsn="monte-buey", layer="testindicies")
+
+# Extraigo los datos, el promedio y el desvio
+datos <- extract(apilado, poligonoextraccion, df=TRUE)
+datos <- datos[-1]
+datos <- datos[-4]
+names(datos) <- c("lai", "mndwi", "ndwi2", "ndwi",  "rvi")
+
+mean.df <- extract(apilado, poligonoextraccion, df=TRUE, fun=mean)
+sd.df <- extract(apilado, poligonoextraccion, df=TRUE, fun=sd)
+
+dataframemediasd <- as.data.frame(cbind(mean.df, sd.df))
+dataframemediasd <- dataframemediasd[-5]
+dataframemediasd <- dataframemediasd[-1]
+dataframemediasd <- dataframemediasd[-6]
+dataframemediasd <- dataframemediasd[-9]
+names(dataframemediasd) <- c("meanlai", "meanmndwi", "meanndwi2", "meanndwi",  "meanrvi", "sdlai", "sdmndwi", "sdndwi2", "sdndwi",  "sdrvi" )
+
+# Calculo la correlacion
+#cor <- rcorr(as.matrix(mean.df), type="spearman")
+write.csv(dataframemediasd, "medias-sdlotes.csv")
+#subset x LAI
+cor(datos[datos$X20160323.lai < 0.7,])
+cor(mean.df)
+
+datoslaialto <- (mean.df[mean.df$X20160323.lai > 0.5,])
+datoslaibajo<- (mean.df[mean.df$X20160323.lai < 0.5,])
+
+
+
+#RVI medias total
+ndwi<-dataframemediasd$meanndwi
+rvi<-dataframemediasd$meanrvi
+sd<-dataframemediasd$sdrvi
+qplot(ndwi,rvi)#+geom_errorbar(aes(ndwi=ndwi, ymin=rvi-sd, ymax=rvi+sd), width=0.05)
+qplot(ndwi,rvi)#+geom_errorbar(aes(ndwi=ndwi, ymin=rvi-sd, ymax=rvi+sd), width=0.05)
+#plotlai <- ggplot(datostotales, aes(x = datos$rvi, y = datos$lai)) + geom_point() + ggtitle("NDVI vs RVI") +labs(x = "NDVI")+labs(y = "RVI")+ 
+
+
+
+
+
